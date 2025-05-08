3270f8630c4e2790ce708a3e88f75ef49bbbc168
Ruhama <ruhamaabraham8@gmail.com>
2025-05-08 11:05:22 +0300

    Refactor: Replace raw tables with ref() to use staging models

--- models/dimensions/dim_currencies.sql
+++ models/dimensions/dim_currencies.sql
@@ -3,5 +3,5 @@
 SELECT
     CurrencyKey,
     CurrencyName
-    -- Add other relevant currency attributes
+    
 FROM {{ ref('stg_adventureworks_currencies') }}
--- models/facts/customer_lifetime_value.sql
+++ models/facts/customer_lifetime_value.sql
@@ -1,8 +1,8 @@
-SELECT
-  c.CustomerKey,
-  c.FirstName,
-  c.LastName,
-  SUM(s.SalesAmount) AS CustomerLifetimeValue
-FROM FactInternetSales s
-JOIN DimCustomer c ON s.CustomerKey = c.CustomerKey
+ SELECT
+    c.CustomerKey,
+    c.FirstName,
+    c.LastName,
+    SUM(s.SalesAmount) AS CustomerLifetimeValue
+FROM {{ ref('stg_adventureworks_internet_sales') }} s
+JOIN {{ ref('stg_adventureworks_customers') }} c ON s.CustomerKey = c.CustomerKey
 GROUP BY c.CustomerKey, c.FirstName, c.LastName
