cfec3d6b083688f053de5d95b0923c4a537cc23e
Ruhama <ruhamaabraham8@gmail.com>
2025-05-14 15:37:36 +0300

    Add view model to aggregate reseller sales by product category (no ORDER BY due to SQL Server view restrictions)

---
+++ models/facts/total_reseller_sales_amount.sql
@@ -0,0 +1,19 @@
+{{config (materialized='view', description=
+    "This table summarizes total reseller sales amount by year, month, and product category."
+  )
+}}
+
+SELECT
+    dpc.EnglishProductCategoryName AS ProductCategory,
+    SUM(frs.SalesAmount) AS TotalSalesAmount
+FROM
+    FactResellerSales frs
+JOIN
+    DimProduct dp ON frs.ProductKey = dp.ProductKey
+JOIN
+    DimProductSubcategory dpsc ON dp.ProductSubcategoryKey = dpsc.ProductSubcategoryKey
+JOIN
+    DimProductCategory dpc ON dpsc.ProductCategoryKey = dpc.ProductCategoryKey
+GROUP BY
+    dpc.EnglishProductCategoryName;;
+
